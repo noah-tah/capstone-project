a6f89f5860547f39a8b6dd5ea3d6c8084270e251
Noah Tah <noahtah21@gmail.com>
2026-07-04 11:04:45 -0500

    basic i/o

--- fancy-sorter.py
+++ fancy-sorter.py
@@ -3,3 +3,15 @@
 # a space between parts of the name is where it would be broken apart.
 
 # The second column contains the material the name will be created with and the third column contains whether it is return to store "Store" or "Shipped" from Art Factory.
+
+from pathlib import Path
+import pandas as pd
+
+def load_orders():
+    file_path = Path(__file__).resolve().parent / "data" / "Name List.xlsx"
+    df = pd.read_excel(file_path, sheet_name="Orders")
+    return df
+
+orders = load_orders()
+print(orders.head())
+print(orders.columns)
